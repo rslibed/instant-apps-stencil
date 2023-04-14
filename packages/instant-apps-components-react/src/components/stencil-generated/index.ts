7e8790c0875616664024c9687aebfbc26b4573f5
Ryan Libed <RLibed@esri.com>
2023-04-14 12:34:06 -0700

    Initial commit.

---
+++ packages/instant-apps-components-react/src/components/stencil-generated/index.ts
@@ -0,0 +1,26 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from '@esri/instant-apps-components';
+
+import { defineCustomElements } from '@esri/instant-apps-components/loader';
+
+defineCustomElements();
+export const InstantAppsExport = /*@__PURE__*/createReactComponent<JSX.InstantAppsExport, HTMLInstantAppsExportElement>('instant-apps-export');
+export const InstantAppsFilterList = /*@__PURE__*/createReactComponent<JSX.InstantAppsFilterList, HTMLInstantAppsFilterListElement>('instant-apps-filter-list');
+export const InstantAppsHeader = /*@__PURE__*/createReactComponent<JSX.InstantAppsHeader, HTMLInstantAppsHeaderElement>('instant-apps-header');
+export const InstantAppsInteractiveLegend = /*@__PURE__*/createReactComponent<JSX.InstantAppsInteractiveLegend, HTMLInstantAppsInteractiveLegendElement>('instant-apps-interactive-legend');
+export const InstantAppsInteractiveLegendCaption = /*@__PURE__*/createReactComponent<JSX.InstantAppsInteractiveLegendCaption, HTMLInstantAppsInteractiveLegendCaptionElement>('instant-apps-interactive-legend-caption');
+export const InstantAppsInteractiveLegendClassic = /*@__PURE__*/createReactComponent<JSX.InstantAppsInteractiveLegendClassic, HTMLInstantAppsInteractiveLegendClassicElement>('instant-apps-interactive-legend-classic');
+export const InstantAppsInteractiveLegendCount = /*@__PURE__*/createReactComponent<JSX.InstantAppsInteractiveLegendCount, HTMLInstantAppsInteractiveLegendCountElement>('instant-apps-interactive-legend-count');
+export const InstantAppsInteractiveLegendLayerCaption = /*@__PURE__*/createReactComponent<JSX.InstantAppsInteractiveLegendLayerCaption, HTMLInstantAppsInteractiveLegendLayerCaptionElement>('instant-apps-interactive-legend-layer-caption');
+export const InstantAppsInteractiveLegendRelationship = /*@__PURE__*/createReactComponent<JSX.InstantAppsInteractiveLegendRelationship, HTMLInstantAppsInteractiveLegendRelationshipElement>('instant-apps-interactive-legend-relationship');
+export const InstantAppsKeyboardShortcuts = /*@__PURE__*/createReactComponent<JSX.InstantAppsKeyboardShortcuts, HTMLInstantAppsKeyboardShortcutsElement>('instant-apps-keyboard-shortcuts');
+export const InstantAppsMeasurement = /*@__PURE__*/createReactComponent<JSX.InstantAppsMeasurement, HTMLInstantAppsMeasurementElement>('instant-apps-measurement');
+export const InstantAppsMeasurementTool = /*@__PURE__*/createReactComponent<JSX.InstantAppsMeasurementTool, HTMLInstantAppsMeasurementToolElement>('instant-apps-measurement-tool');
+export const InstantAppsPopover = /*@__PURE__*/createReactComponent<JSX.InstantAppsPopover, HTMLInstantAppsPopoverElement>('instant-apps-popover');
+export const InstantAppsPopovers = /*@__PURE__*/createReactComponent<JSX.InstantAppsPopovers, HTMLInstantAppsPopoversElement>('instant-apps-popovers');
+export const InstantAppsScoreboard = /*@__PURE__*/createReactComponent<JSX.InstantAppsScoreboard, HTMLInstantAppsScoreboardElement>('instant-apps-scoreboard');
+export const InstantAppsSocialShare = /*@__PURE__*/createReactComponent<JSX.InstantAppsSocialShare, HTMLInstantAppsSocialShareElement>('instant-apps-social-share');
